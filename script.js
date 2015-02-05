2bf108ea3bb5af88bed46bbbba7a0a694b30cdd7
lasha <lashacst@gmail.com>
2015-02-05 20:20:59 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+(function() {
+     'use strict'; 
+  var canvas = new fabric.Canvas('tictactoe');
+
+  canvas.setWidth(window.innerWidth);
+  canvas.setHeight(window.innerHeight);
+
+  var x = canvas.getWidth() / 3;
+  var y = canvas.getHeight() / 3;
+  x = x - (x - y);
+
+  var makeLine = function(coords) {
+    return new fabric.Line(coords, {
+      stroke: 'black',
+      strokeWidth: 1,
+    }); 
+  }
+
+  var makeGroup = function(groups) {
+    return new fabric.Group(groups, {
+      selectable: true,
+      hoverCursor: 'pointer',
+      hasBorders: false,
+      hasControls: false,
+      lockMovementX: true,
+      lockMovementY: true
+    });
+  }
+
+  canvas.add(
+    makeGroup([makeLine([x, 0, x, y]), makeLine([0, y, x, y])]),
+    makeGroup([makeLine([x * 2, 0, x * 2, y]), makeLine([x * 2, y, x, y])]),
+    makeGroup([makeLine([x * 3, 0, x * 3, y]), makeLine([x * 3, y, x * 2, y])]),
+    makeGroup([makeLine([x, y, x, y * 2]), makeLine([0, y * 2, x, y * 2])]),
+    makeGroup([makeLine([x * 2, y, x * 2, y * 2]), makeLine([x, y * 2, x * 2, y * 2])]),
+    makeGroup([makeLine([x * 3, y, x * 3, y * 2]), makeLine([x * 3, y * 2, x * 2, y * 2])]),
+    makeGroup([makeLine([x, y * 2, x, y * 3]), makeLine([0, y * 3, x, y * 3])]),
+    makeGroup([makeLine([x * 2, y * 2, x * 2, y * 3]), makeLine([x, y * 3, x * 2, y * 3])]),
+    makeGroup([makeLine([x * 3, y * 2, x * 3, y * 3]), makeLine([x * 2, y * 3, x * 3, y * 3])])
+    );
+
+  // canvas.forEachObject(function(o) {
+  //   o.on('mouse:down', function(e) {
+  //     alert();
+  //   })
+  // });
+
+  canvas.on({
+    'mouse:down': function(e) {
+      if (e.target) {
+        var box = e.target;
+        var w = box.getWidth();
+        var h = box.getHeight();
+
+        console.log(w);
+      }
+    }
+  });
+
+  canvas.renderAll();
+})()
