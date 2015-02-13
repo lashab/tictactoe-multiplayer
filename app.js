e1a23fa56c02f486d7520b43677d64b9fe53f652
lasha <lashacst@gmail.com>
2015-02-13 18:13:57 +0400

    added mongoose initialize schemas

--- app.js
+++ app.js
@@ -3,17 +3,18 @@
  * Module dependencies.
  */
 var express = require('express')
-  , routes = require('./routes')
+  , routes = require('./app/routes')
   , http = require('http')
   , path = require('path');
 
 var app = express();
 var sio = require('http').Server(app);
 var io = require('socket.io')(sio);
+var Schema = require('./app/models/schema');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
